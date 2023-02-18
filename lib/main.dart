3b0ff640c7095c36165ec2e7b02e9a98ba9fee8d
user <username@example.com>
2023-02-18 20:01:49 +0545

    Added riverpod, theme settings and routes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image/models/theme_settings.dart';
+import 'package:image/routes/routes.dart';
 
-import 'pages/image_page.dart';
+final themeProvider = StateNotifierProvider<ThemeSettingsNotifier, bool>((ref) => ThemeSettingsNotifier());
 
 void main() {
   // Dark navigation bar. Code may give issues.
@@ -12,156 +14,22 @@ void main() {
       systemNavigationBarColor: Colors.black,
     ));
   }
-
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var darkMode = ref.watch(themeProvider);
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const MainPage(),
+      // darkTheme: ThemeData.dark(),
+      theme: darkMode ? darkTheme : lightTheme,
+      // home: const MainPage(),
+      initialRoute: RouteManager.mainPage,
+      onGenerateRoute: RouteManager.generateRoute,
     );
   }
 }
 
-class MainPage extends StatefulWidget {
-  final int? index;
-
-  const MainPage({this.index, super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  List files = [File('')];
-
-  // RangeValues rangeValues = RangeValues(0, 1);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(),
-
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            toolbarHeight: MediaQuery.of(context).size.height * 0.08,
-          ),
-          SliverFillRemaining(
-            child: PageView.builder(
-              // scrollDirection: Axis.vertical,
-              itemCount: files.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return InteractiveViewer(
-                              child: ImagePage(image: files[index]));
-                        },
-                      ),
-                    );
-                  },
-                  child: Image.file(files[index]),
-                );
-              },
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: BottomNavigationBar(
-              items: bottomBarItems(),
-            ),
-          ),
-        ],
-      ),
-
-      drawer: SafeArea(
-        child: Drawer(
-          child: Column(
-            children: [
-              // For folder
-              ListTile(
-                onTap: () async {
-                  String? result = await FilePicker.platform.getDirectoryPath();
-
-                  if (result != null) {
-                    setState(
-                      () {
-                        files.clear();
-                        Directory(result).listSync().forEach(
-                          (f) {
-                            if (f is File && f.path.endsWith('.jpg') ||
-                                f.path.endsWith('.jpeg') ||
-                                f.path.endsWith('.png') ||
-                                f.path.endsWith('.gif')) {
-                              files.add(f);
-                            }
-                          },
-                        );
-                      },
-                    );
-                  }
-                  Navigator.pop(context);
-                },
-                leading: const Icon(
-                  Icons.add,
-                  size: 25,
-                ),
-                title: const Text('Open folder'),
-              ),
-              // For individual or multiple files
-              ListTile(
-                onTap: () async {
-                  FilePickerResult? result = await FilePicker.platform
-                      .pickFiles(type: FileType.image, allowMultiple: true);
-
-                  if (result != null) {
-                    setState(
-                      () {
-                        files.clear();
-                        files = result.paths
-                            .map((path) => File(path.toString()))
-                            .toList();
-                      },
-                    );
-                  }
-                  Navigator.pop(context);
-                },
-                leading: const Icon(
-                  Icons.add,
-                  size: 25,
-                ),
-                title: const Text('Open files'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  List<BottomNavigationBarItem> bottomBarItems() {
-    return [
-      const BottomNavigationBarItem(
-        label: 'Home',
-        icon: Icon(
-          Icons.home,
-          color: Colors.white,
-        ),
-        backgroundColor: Colors.amber,
-      ),
-      const BottomNavigationBarItem(
-        label: 'Add',
-        icon: Icon(Icons.add, color: Colors.white),
-        backgroundColor: Colors.amber,
-      ),
-    ];
-  }
-}
---
+++ lib/models/theme_settings.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+ThemeData lightTheme = ThemeData.light();
+
+ThemeData darkTheme = ThemeData.dark();
+
+class ThemeSettingsNotifier extends StateNotifier<bool> {
+  ThemeSettingsNotifier() : super(false) {
+    initializePrefs();
+  }
+
+  late SharedPreferences preferences;
+
+  Future initializePrefs() async {
+    preferences = await SharedPreferences.getInstance();
+    var darkMode = preferences.getBool("darkMode");
+    state = darkMode ?? true;
+  }
+
+  void toggle() async {
+    state = !state;
+    preferences.setBool("darkMode", state);
+  }
+}
